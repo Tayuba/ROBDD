f04a1649640d98e20ffe0dad1409579537d4048a
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-12-28 23:45:33 +0100

    cmake build

---
+++ cmake-build-debug/CMakeFiles/VDSProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VDSProject.dir/src/main.cpp.obj"
+  "VDSProject.exe"
+  "VDSProject.exe.manifest"
+  "VDSProject.pdb"
+  "libVDSProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VDSProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/src/test/CMakeFiles/VDSProject_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VDSProject_test.dir/main_test.cpp.obj"
+  "VDSProject_test.exe"
+  "VDSProject_test.exe.manifest"
+  "VDSProject_test.pdb"
+  "libVDSProject_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VDSProject_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
