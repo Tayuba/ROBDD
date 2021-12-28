e47b70fc51a0e428fa578e33d58fa19537291306
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-12-29 00:41:27 +0100

    Update cmake_install.cmake

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/ayuba/CLionProjects/VDSProject_Group4/cmake-build-debug/_deps/googletest-build/cmake_install.cmake")
+  include("C:/Users/ayuba/CLionProjects/ROBDD/cmake-build-debug/_deps/googletest-build/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
@@ -55,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ayuba/CLionProjects/VDSProject_Group4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ayuba/CLionProjects/ROBDD/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
