37c1b6e73852745e8f1ab72c5bd011f63e45ba31
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-12-29 03:12:16 +0100

    Update cmake_install.cmake

--- cmake-build-debug/_deps/googletest-build/cmake_install.cmake
+++ cmake-build-debug/_deps/googletest-build/cmake_install.cmake
@@ -1,4 +1,5 @@
-# Install script for directory: C:/Users/ayuba/CLionProjects/VDSProject_Group4/cmake-build-debug/_deps/googletest-src
+# Install script for directory: C:/Users/ayuba/CLionProjects/
+/cmake-build-debug/_deps/googletest-src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +40,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/ayuba/CLionProjects/VDSProject_Group4/cmake-build-debug/_deps/googletest-build/googlemock/cmake_install.cmake")
+  include("C:/Users/ayuba/CLionProjects/ROBDD/cmake-build-debug/_deps/googletest-build/googlemock/cmake_install.cmake")
 
 endif()
 
